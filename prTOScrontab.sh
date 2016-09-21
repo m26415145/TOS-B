6e94c466d06a1dab2220855e0504b6b03083a38e
m26415145 <m26415145@john.petra.ac.id>
2016-09-21 22:24:12 +0700

    tugas crontab

---
+++ prTOScrontab.sh
@@ -0,0 +1,5 @@
+0 11,18 * * 1-5 /home/tos-b/emas.txt
+0 8-18 * * 1-5 /home/tos-b/bcaSGD.txt
+0 8-18 * * 1-5 /home/tos-b/bcaUSD.txt
+0 8-18 * * 1-5 /home/tos-b/mandiriSGD.txt
+0 8-18 * * 1-5 /home/tos-b/mandiriUSD.txt
